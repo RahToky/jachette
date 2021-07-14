2abb26605cd312a69dfc6e60e2f175672d0f3244
Mahatoky <mahatoky.rasolonirina@pulse.mg>
2021-07-14 22:19:56 +0300

    listtile commandes

--- lib/entity/cards_entity.dart
+++ lib/entity/cards_entity.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Cards {
-
   int id;
   String type;
   String number;
 
-  Cards({@required this.id,@required this.type, @required this.number});
-
+  Cards({@required this.id, @required this.type, @required this.number});
 }
--- lib/ui/screen/list/menu_detail_screen.dart
+++ lib/ui/screen/list/menu_detail_screen.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
 class MenuDetailScreen extends StatelessWidget {
-
   static final String routeName = "/commands";
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
         body: Column(
           children: [
             Container(
-              child: Text('Titre'),
-            )
+              alignment: Alignment.center,
+              child: Text('Commandes', textScaleFactor: 1.8),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 50,
+                physics: BouncingScrollPhysics(),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
+                  color: Colors.transparent,
+                ),
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Image.asset('assets/images/photo_1.png'),
+                    title: Text('title $index'),
+                    subtitle: Text('sous titre $index \nblabla'),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
